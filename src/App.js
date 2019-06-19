a79b83bd39440eb2b44a2d3b591458f375f968e2
Michael Snowden <mtsnowden@gmail.com>
2019-06-19 13:22:58 -0500

    yayyy got the board on the DOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Board from "./script"
+import './App.css'
 
 
 class App extends Component {
@@ -14,26 +16,13 @@ class App extends Component {
       //   <div className="cell"></div>
       //   <div id="announce-winner"></div>
       // </div>
-      <div id="board">
-        <div className="row">
-          <div data-cell="0"></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
-        </div>
-        <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
-        </div>
-        <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
-        </div>
-        <button id="clear">Clear Board</button>
-
-    </div>
+      
+      <div className='container'>
+        <Board />
+      </div>
     );
   }
 }
 export default App;
+
+// ReactDOM.render(<TicTacToe />, document.getElementById('tic-tac-toe'));
--- src/script.js
+++ src/script.js
@@ -1,40 +1,67 @@
-'use strict';
 
-$(document).ready(function () {
-  var turn = 'X';
-  $('[data-cell]').on('click', function () {
+import React, { Component } from 'react';
+
+// $(document).ready(function () {
+//   var turn = 'X';
+//   $('[data-cell]').on('click', function () {
     
-    if ($(this).text() === ''){
-      $(this).text(turn);
-      checkForWin();
-      if(turn === 'O'){
-        turn = 'X'; 
-      } else {
-      turn = 'O';
-      }
+//     if ($(this).text() === ''){
+//       $(this).text(turn);
+//       checkForWin();
+//       if(turn === 'O'){
+//         turn = 'X'; 
+//       } else {
+//       turn = 'O';
+//       }
+//     }
+//   })
+
+
+
+// function checkForWin(){
+//   if($('[data-cell="0"]').text() === turn && $('[data-cell="1"]').text() === turn && $('[data-cell="2"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="3"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="5"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="6"]').text() === turn && $('[data-cell="7"]').text() === turn && $('[data-cell="8"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="0"]').text() === turn && $('[data-cell="3"]').text() === turn && $('[data-cell="6"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="1"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="7"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="2"]').text() === turn && $('[data-cell="5"]').text() === turn && $('[data-cell="8"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="0"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="8"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   } else if ($('[data-cell="2"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="6"]').text() === turn) {
+//     $('#announce-winner').text(`${turn} wins`);
+//   }
+// }
+
+// })
+
+class Board extends Component {
+    render(){
+        return (
+            <div>
+        <div className="row">
+          <div data-cell="0"></div>
+          <div data-cell="1"></div>
+          <div data-cell="2"></div>
+        </div>
+        <div className="row">
+          <div data-cell="3"></div>
+          <div data-cell="4"></div>
+          <div data-cell="5"></div>
+        </div>
+        <div className="row">
+          <div data-cell="6"></div>
+          <div data-cell="7"></div>
+          <div data-cell="8"></div>
+        </div>
+      </div>
+        )
     }
-  })
-
-
-
-function checkForWin(){
-  if($('[data-cell="0"]').text() === turn && $('[data-cell="1"]').text() === turn && $('[data-cell="2"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="3"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="5"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="6"]').text() === turn && $('[data-cell="7"]').text() === turn && $('[data-cell="8"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="0"]').text() === turn && $('[data-cell="3"]').text() === turn && $('[data-cell="6"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="1"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="7"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="2"]').text() === turn && $('[data-cell="5"]').text() === turn && $('[data-cell="8"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="0"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="8"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  } else if ($('[data-cell="2"]').text() === turn && $('[data-cell="4"]').text() === turn && $('[data-cell="6"]').text() === turn) {
-    $('#announce-winner').text(`${turn} wins`);
-  }
 }
 
-})
+module.exports = Board
